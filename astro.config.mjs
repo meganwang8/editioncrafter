730ed55a94a11b85ce86e2d55944901b984e2c72
megan <99512458+meganwang8@users.noreply.github.com>
2026-02-21 04:55:59 +0000

    Modify astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  site: 'https://meganwang8.github.io',
   base: '/editioncrafter',
 });
